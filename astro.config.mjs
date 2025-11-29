1841574114141f1cf98bdc10d8d4b1cac411b59e
dmytro <dmytro.anastasiy@student.howest.be>
2025-11-29 03:41:30 +0100

    Fix site configuration for deploy repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-github-username.github.io',
-  base: '/cck/',
+  site: 'https://dmytroanastasiy.github.io',
+  base: '/deploy/',
 });
